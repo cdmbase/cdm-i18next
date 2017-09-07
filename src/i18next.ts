699e3d2a69627c0cab06b485063bb332b6e7417a
veeramarni <mvasireddy@cdmbase.com>
2017-09-07 07:09:14 -0400

    initial checkin

---
+++ src/i18next.ts
@@ -0,0 +1,33 @@
+
+
+export const nls = {
+    overloadTranslationOptionHandler: function(args) {
+        var asArray = Array.prototype.slice.call(args);
+        var key = asArray.shift();
+        var defaultValue = asArray.shift();
+
+        return {
+            defaultValue: defaultValue,
+            nls: asArray,
+            postProcess: 'nls',
+        }
+    },
+    processor: {
+        name: 'nls',
+        type: 'postProcessor',
+
+        process(value, key, options) {
+            console.warn(value, options);
+            if(!options.nls) {
+                return value;
+            }
+
+            options.nls.forEach(function(newValue, index) {
+                var searchPattern = new RegExp('\\{' + index + '\\}');
+                value = value.replace(searchPattern, newValue);
+            });
+
+            return value;
+        }
+    }
+}
