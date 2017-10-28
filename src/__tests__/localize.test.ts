3ba0ab4c727345970afb316e5a37c3e741115414
veeramarni <mvasireddy@cdmbase.com>
2017-10-28 19:53:02 -0400

    update tsconfig

--- src/__tests__/localize.test.ts
+++ src/__tests__/localize.test.ts
@@ -1,11 +1,11 @@
-import i18next from 'i18next';
+import * as i18next from 'i18next';
 import 'jest';
-import * as nls from '../i18next';
+import { nls } from '../i18next';
 
 
 describe('Localize', () => {
     it('Simple call', () => {
-        let localize = i18next({ locale: 'de-DE'})();
+        let localize = i18next({ locale: 'de-DE' })();
         let message = 'Hello World';
         expect(localize.t('key', message)).toEqual(message);
     })
